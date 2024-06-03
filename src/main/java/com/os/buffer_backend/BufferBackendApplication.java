19a7fca6f0885618fae4e679199cd8c80d7d2842
HAN <QQ1554646899@outlook.com>
2024-06-03 14:35:11 +0800

    提交了yml文件

--- src/main/java/com/os/buffer_backend/BufferBackendApplication.java
+++ src/main/java/com/os/buffer_backend/BufferBackendApplication.java
@@ -1,9 +1,11 @@
 package com.os.buffer_backend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.os.BufferBackend.Mapper")
 public class BufferBackendApplication {
 
     public static void main(String[] args) {
