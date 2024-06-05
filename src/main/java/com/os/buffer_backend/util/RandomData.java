513d2574527411ccf53b43fac5c65653d20aa4fa
HAN <QQ1554646899@outlook.com>
2024-06-05 10:11:03 +0800

    提交了随机生成数据

---
+++ src/main/java/com/os/buffer_backend/util/RandomData.java
@@ -0,0 +1,19 @@
+package com.os.buffer_backend.util;
+
+import java.security.SecureRandom;
+
+public class RandomData {
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    private static SecureRandom random = new SecureRandom();
+
+    public static String generateRandomString() {
+        int length = 10;
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            sb.append(CHARACTERS.charAt(random.nextInt(CHARACTERS.length())));
+        }
+        System.out.println("Random String: " + sb.toString());
+        return sb.toString();
+    }
+
+}
